6c29e3a400bd3b0e5d79629281dc23f4877b6400
Y. H. Janith Madusanka Kulathunga <152203226+Janithjm99@users.noreply.github.com>
2025-02-13 15:52:06 +0530

    Add files via upload

---
+++ app/Http/Controllers/InquiryController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Inquiry;
+
+class InquiryController extends Controller
+{
+    public function store(Request $request)
+    {
+        $request->validate([
+            'first_name' => 'required|string|max:255',
+            'last_name' => 'required|string|max:255',
+            'email' => 'required|email',
+            'subject' => 'required|string|max:255',
+            'message' => 'required|string',
+        ]);
+
+        Inquiry::create($request->all());
+
+        return redirect()->back()->with('success', 'Inquiry submitted successfully!');
+    }
+}
